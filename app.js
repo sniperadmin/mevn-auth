04410567b813e9a1d96e34497305c350b4797888
Nasr Galal <nasrbue@gmail.com>
2020-06-09 04:00:13 +0200

    initialize server config

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const app = express();
+
+//middlewares
+
+// form data
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// json body
+app.use(bodyParser.json());
+
+// cors
+app.use(cors());
+
+// setting up static dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+// strategy
+require('./config/passport')(passport);
+
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log('connected to database') })
+  .catch(err => console.log('Could not connect to database', err));
+
+
+const users = require('./routes/api/users');
+
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log('server listening on ' + port); })
