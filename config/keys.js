7e7a8622576d03ee7eab2c43b729dce7d62d4096
Nasr Galal <nasrbue@gmail.com>
2020-06-09 15:51:00 +0200

    feat: modify database configuration

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,13 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/mevn",
-  secret: "secret"
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    mongoURI: "mongodb+srv://nasr:099013090@vue-qynta.mongodb.net/mevn-auth?retryWrites=true&w=majority",
+    secret: "secret"
+  }
+} else {
+  module.exports = {
+    mongoURI: "mongodb://localhost:27017/mevn",
+    secret: "secret"
+  }
+
 }
