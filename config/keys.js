f1b1cda5e4adfc172756057c3827a792827d02d6
Nasr Galal <nasrbue@gmail.com>
2020-06-09 16:07:50 +0200

    bug: fix db connection

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,4 @@
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: "mongodb+srv://nasr:099013090@vue-qynta.mongodb.net/mevn-auth?retryWrites=true&w=majority",
-    secret: "secret"
-  }
-} else {
-  module.exports = {
-    mongoURI: "mongodb://localhost:27017/mevn",
-    secret: "secret"
-  }
-
+module.exports = {
+  mongoURI: "mongodb+srv://nasr:099013090@vue-qynta.mongodb.net/mevn-auth?retryWrites=true&w=majority",
+  secret: "secret"
 }
